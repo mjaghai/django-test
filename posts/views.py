86975655e5d07400ecd83fa80455b1fefaa1d299
James <mohammadjavadpoudinehaghai@gmail.com>
2026-09-22 02:00:50 +0330

    style: lint using ruff

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.shortcuts import render,get_object_or_404
-from .models import Post
+
 
 def post_list(request):
-    return render(request,'posts/posts.html')
+    return render(request, "posts/posts.html")
+
 
 def post_detail(request):
 
-
-    
-    return render(request,'posts/post.html')
+    return render(request, "posts/post.html")
